cdf5b759fd6439cd678cd135b545bf4e1bc77105
ViacheslavZN <slava19887@yandex.ru>
2023-03-16 17:59:48 +0300

    lesson_3

--- wsgi.py
+++ wsgi.py
@@ -6,3 +6,19 @@ if __name__ == "__main__":
         debug=True,
     )
 
+
+@app.cli.command("init-db")
+def init_db():
+    db.create_all()
+    print("done!")
+
+
+@app.cli.command("create-users")
+def create_users():
+    from blog.models import User
+    admin = User(username="admin", is_staff=True)
+    james = User(username="james")
+    db.session.add(admin)
+    db.session.add(james)
+    db.session.commit()
+    print("done! created users:", admin, james)
