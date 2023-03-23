c2ee5cc6238605cb2dee0fce173b334dee67b07c
ViacheslavZN <slava19887@yandex.ru>
2023-03-23 16:35:45 +0300

    lesson_5

--- blog/app.py
+++ blog/app.py
@@ -1,3 +1,4 @@
+import app as app
 from flask import Flask, render_template
 from blog.views.users import users_app
 from blog.views.articles import articles_app
@@ -5,8 +6,11 @@ from blog.models.database import db
 from blog.views.auth import login_manager, auth_app
 import os
 from flask_migrate import Migrate
+from blog.security import flask_bcrypt
 
-Migrate=Migrate(app,db)
+flask_bcrypt.init_app(app)
+
+migrate = Migrate(app, db)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////tmp/blog.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
@@ -15,8 +19,6 @@ db.init_app(app)
 app.config["SECRET_KEY"] = "abcdefg123456"
 app.register_blueprint(auth_app, url_prefix="/auth")
 login_manager.init_app(app)
-
-
 
 app = Flask(__name__)
 
@@ -30,3 +32,12 @@ app.config.from_object(f"blog.configs.{cfg_name}")
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.cli.command("create-admin")
+def create_admin():
+    from blog.models import User
+    admin = User(username="admin", is_staff=True)
+    admin.password = os.environ.get("ADMIN_PASSWORD") or "adminpass"
+    db.session.add(admin)
+    db.session.commit()
+    print("created admin:", admin)
